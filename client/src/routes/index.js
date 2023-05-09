1318edf93294017c0a0a146280c90265d91fa9fe
HuuHunga9 <91863327+HuuHunga9@users.noreply.github.com>
2023-05-09 10:52:32 +0700

    Update index.js

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,4 +1,6 @@
-import { HeaderOnly } from '~/components/Layout';
+import CapNhatLop from '~/pages/CapNhatLop';
+import CapNhatNhomLop from '~/pages/CapNhatNhomLop';
+import CapNhatNhuCauHoc from '~/pages/CapNhatNhuCauHoc';
 import Home from '~/pages/Home';
 import Login from '~/pages/Login';
 import Upload from '~/pages/Upload';
@@ -6,11 +8,15 @@ import Upload from '~/pages/Upload';
 // Không cần đăng nhập vẫn xem được
 const publicRouter = [
     { path: '/', component: Home },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/upload', component: Upload },
     { path: '/login', component: Login, layout: null },
+    { path: '/capnhatlop', component: CapNhatLop },
+    { path: '/capnhatnhomlop', component: CapNhatNhomLop },
+    { path: '/capnhatnhucauhoc', component: CapNhatNhuCauHoc },
 ];
 
 // Đăng nhập mới xem được
 const privateRouter = [];
 
 export { publicRouter, privateRouter };
+
